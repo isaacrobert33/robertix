59025ffee7c7f00f2530f1ebbcecfa11a7bba2c3
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-04-07 15:10:13 +0100

    Newsletter setup

--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -3,8 +3,8 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { Newsletter } from "./Newsletter";
 
 export const MailchimpForm = () => {
-  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
-
+  const postUrl = "https://gmail.us13.list-manage.com/subscribe/post?u=32aa26690a7756a5fa5f8497a&amp;id=8bec678153&amp;f_id=00df93e2f0"
+  // `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
   return (
     <>
       <MailchimpSubscribe
