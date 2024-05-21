912566f5d73ea4de12ad29ef9d3c50aac69a8542
Isaac Robert <owoyemiisaac33@gmail.com>
2024-05-21 10:29:29 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -55,41 +55,18 @@ export const Projects = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
                 <p>Want to throw a look at my works? I've worked on diverse projects and these are a few of the multiple ones:</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p></p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p></p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
+                <Row>
+                  {
+                    projects.map((project, index) => {
+                      return (
+                        <ProjectCard
+                          key={index}
+                          {...project}
+                          />
+                      )
+                    })
+                  }
+                </Row>
               </div>}
             </TrackVisibility>
           </Col>
